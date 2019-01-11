fa3346ebae0b60df74e4e38be38e5dd1901235b3
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-10 20:43:32 -0800

    Change value of pain property in package.json to ./src/app.js. Configure webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+
+            { test: /\.html$/, loader: "html-loader" }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
