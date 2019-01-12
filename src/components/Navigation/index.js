83505e2a31e6c89892a85490ec0cae5cbd426e0c
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-12 08:46:46 -0800

    Wrap each link in a Link component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
         <div>
-            <h2>Home</h2>
-            <h2>Work</h2>
-            <h2>About Me</h2>
-            <h2>Contact</h2>
+            <Link to="/">
+                <h2>Home</h2>
+            </Link>
+            <Link to="/work">
+                <h2>Work</h2>
+            </Link>
+            <Link to="/about">
+                <h2>About me</h2>
+            </Link>
+            <Link to="/contact">
+                <h2>Contact</h2>
+            </Link>
         </div>
     );
 };
