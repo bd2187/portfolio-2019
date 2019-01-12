1ad7f6b6aa8f507845d8f001b9a7a2461733627f
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-12 08:28:59 -0800

    Initial commit. Render component. Export components to be used in config/routes.js

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+            <h2>Work</h2>
+            <h2>About Me</h2>
+            <h2>Contact</h2>
+        </div>
+    );
+};
+
+export default Navigation;
