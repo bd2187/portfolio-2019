7915ecaebe4cabbaba84e3f442e0c0038b8c10f8
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-12 08:47:11 -0800

    Remove the Navigation component

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,9 @@ import React from "react";
 
 import routes from "../config/routes";
 import Wrapper from "./Wrapper";
-import Navigation from "./Navigation";
 
 const App = () => {
-    return (
-        <Wrapper>
-            <Navigation />
-            {routes}
-        </Wrapper>
-    );
+    return <Wrapper>{routes}</Wrapper>;
 };
 
 export default App;
