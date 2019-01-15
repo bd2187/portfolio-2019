3dc7e8335a05886788126bafce703764028a96f0
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-14 22:54:59 -0800

    Import normalize and main styles

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import normalize from "./public/css/normalize.css";
+import main from "./public/css/main.css";
 
 render(<App />, document.getElementById("root"));
