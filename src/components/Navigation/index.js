d9e7138c8b1f581d29ccdcd6907a3f970d9768b4
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-14 22:54:07 -0800

    Change Navigation component from a functional to class component. Give it a state object with a isNavOpen property. Render the Navigation menu

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
+import styles from "./Navigation.css";
 
-const Navigation = () => {
-    return (
-        <div>
-            <Link to="/">
-                <h2>Home</h2>
-            </Link>
-            <Link to="/work">
-                <h2>Work</h2>
-            </Link>
-            <Link to="/about">
-                <h2>About me</h2>
-            </Link>
-            <Link to="/contact">
-                <h2>Contact</h2>
-            </Link>
-        </div>
-    );
-};
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        };
+
+        this.onHamburgerClicked = this.onHamburgerClicked.bind(this);
+        this.closeNav = this.closeNav.bind(this);
+    }
+
+    onHamburgerClicked() {
+        this.setState(state => ({ isNavOpen: !state.isNavOpen }));
+    }
+
+    closeNav() {
+        this.setState({ isNavOpen: false });
+    }
+
+    render() {
+        const isNavOpen = this.state.isNavOpen;
+        return (
+            <Fragment>
+                <div
+                    className={styles["fixed-header"]}
+                    onClick={this.onHamburgerClicked}
+                >
+                    <div className={styles["hamburger-menu"]}>burger</div>
+                </div>
+                <div
+                    className={`${styles["navigation-menu"]} ${
+                        isNavOpen ? styles["nav-open"] : styles["nav-closed"]
+                    }`}
+                >
+                    <Link to="/" onClick={this.closeNav}>
+                        <h2 className={styles["nav-option"]}>Home</h2>
+                    </Link>
+                    <Link to="/work" onClick={this.closeNav}>
+                        <h2 className={styles["nav-option"]}>Work</h2>
+                    </Link>
+                    <Link to="/about" onClick={this.closeNav}>
+                        <h2 className={styles["nav-option"]}>About me</h2>
+                    </Link>
+                    <Link to="/contact" onClick={this.closeNav}>
+                        <h2 className={styles["nav-option"]}>Contact</h2>
+                    </Link>
+                </div>
+            </Fragment>
+        );
+    }
+}
 
 export default Navigation;
