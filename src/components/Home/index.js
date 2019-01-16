b58becc782dc63d80cf4ab8c955cf6e716d694d2
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-15 22:19:56 -0800

    Render and style text on homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./Home.css";
 
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div className={styles["title-wrap"]}>
+            <h1>
+                Hi, I'm Brandon! I'm a{" "}
+                <span style={{ color: "#1E6DFF" }}>front-end developer</span>{" "}
+                from Los Angeles who is currently working at VS Media.
+            </h1>
+        </div>
+    );
 };
 
 export default Home;
