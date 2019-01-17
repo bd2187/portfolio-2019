f7670e1dd61ea8beed9c3e2f7b19889620dffb95
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-16 22:57:33 -0800

    Iterate through workCollection array and render data to DOM

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import workCollection from "../../config/work";
+
+var renderWorkItem = item => {
+    // return <li key={item.title}>{item.title}</li>;
+
+    return (
+        <li key={item.seoTitle}>
+            <Link to={`work/${item.seoTitle}`}>{item.title}</Link>
+        </li>
+    );
+};
 
 const Work = () => {
-    return <h1>Work</h1>;
+    var professionalWork = [];
+    var personalWork = [];
+
+    workCollection.forEach(work => {
+        work.professional
+            ? (professionalWork = [...professionalWork, work])
+            : (personalWork = [...personalWork, work]);
+    });
+
+    return (
+        <Fragment>
+            <h1>Professional</h1>
+            <ul>{professionalWork.map(renderWorkItem)}</ul>
+
+            <h1>Personal</h1>
+            <ul>{personalWork.map(renderWorkItem)}</ul>
+        </Fragment>
+    );
 };
 
 export default Work;
