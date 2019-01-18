4779c9015079d8bfd882712fffcadc5298c6b513
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-17 23:02:21 -0800

    Import SoloWork component and render it when user visits the work route with a parameter present

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "../components/Home";
 import Work from "../components/Work";
+import SoloWork from "../components/Work/SoloWork";
 import About from "../components/About";
 import Contact from "../components/Contact";
 
@@ -15,7 +16,7 @@ const routes = (
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/work" component={Work} />
-                <Route exact path="/work/:workTitle" component={Work} />
+                <Route exact path="/work/:workTitle" component={SoloWork} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/contact" component={Contact} />
             </Switch>
