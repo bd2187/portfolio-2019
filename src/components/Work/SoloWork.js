1486cb49325a8fec528014ac50b823a12a772a08
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-12 21:11:18 -0800

    Render sample description and gif. Render project title and links

--- src/components/Work/SoloWork.js
+++ src/components/Work/SoloWork.js
@@ -1,5 +1,8 @@
 import React from "react";
 import workCollection from "../../config/work";
+import { Link } from "react-router-dom";
+
+import styles from "./Work.css";
 
 const SoloWork = props => {
     var projectTitleFromUrl = props.match.params.workTitle;
@@ -11,12 +14,46 @@ const SoloWork = props => {
             : null;
     })[0];
 
+    const { title, link, github } = workInfo;
+
     if (!workInfo) {
         return <h1>Could not find project</h1>;
     }
 
     // Render data here
-    return <h1>Solo Work</h1>;
+    return (
+        <div className={styles["solo-project-container"]}>
+            <h1 className={styles["project-title"]}>{title}</h1>
+            <div className={styles["project-content-container"]}>
+                <p className={styles["project-description"]}>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.{" "}
+                </p>
+                <img
+                    className={styles["project-gif"]}
+                    src="https://media.giphy.com/media/14mcdRwHEgEG3u/giphy.gif"
+                />
+            </div>
+            <div className={styles["project-links"]}>
+                {github ? (
+                    <a href={github} target="_blank">
+                        View Code
+                    </a>
+                ) : null}
+
+                <a href={link} target="_blank">
+                    View Website
+                </a>
+            </div>
+            <Link to="/work" className={styles["view-more-projects"]}>
+                <i class="fa fa-arrow-left" aria-hidden="true" />
+                View more projects
+            </Link>
+        </div>
+    );
 };
 
 export default SoloWork;
