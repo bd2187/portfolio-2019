7d6c6e34d235ba5eb5b2b8307fa814564449171a
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-20 22:29:27 -0800

    Create fn, displayShortDescription.

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -3,6 +3,17 @@ import { Link } from "react-router-dom";
 import styles from "./Work.css";
 import workCollection from "../../config/work";
 
+function displayShortDescription(text) {
+    var textArr = text.split("");
+    var maxCharacters = 90;
+
+    if (textArr.length > maxCharacters) {
+        return textArr.slice(0, maxCharacters).join("") + "...";
+    } else {
+        return text;
+    }
+}
+
 var renderWorkItem = item => {
     return (
         <li key={item.seoTitle} className={styles["project-thumbnail"]}>
@@ -16,8 +27,7 @@ var renderWorkItem = item => {
                 <div className={styles["project-info-container"]}>
                     <h4 className={styles["project-title"]}>{item.title}</h4>
                     <p className={styles["project-description"]}>
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry.
+                        {displayShortDescription(item.description)}
                     </p>
                 </div>
             </Link>
